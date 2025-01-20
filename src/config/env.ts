187a59d0f655607158337a1737779e585ac96d7b
MuhamedReswan <muhamedreswan9917@gmail.com>
2025-01-20 11:33:59 +0530

    feat: added cors for cross-origin request

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,9 @@
 
+import exp from "constants";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const MONGODBURL = process.env.MONGODBURL
 export const PORT = process.env.PORT
 export const REDISURL = process.env.REDISURL
+export const CLIENTURL = process.env.CLIENTURL
